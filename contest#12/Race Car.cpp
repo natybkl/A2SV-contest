bf8cefbf34f1e2413e17457f5ea9344ea9ee077b
Natnael bekele <62074050+natybkl@users.noreply.github.com>
2021-03-18 14:44:32 +0300

    Update Race Car.cpp

--- contest#12/Race Car.cpp
+++ contest#12/Race Car.cpp
@@ -6,39 +6,29 @@ public:
         q.push({0, 0, 1}); // level, position, Speed
 
         while(!q.empty()) {
-
             auto car = q.front();
-
             q.pop();
 
             level = car[0];
-
             speed = car[2];
-
             position = car[1];
          
             if(position == target) return level;
             
             // wondering if accelerating would get us to target
             if(position + speed <= 10000 && position + speed > 0)
-
                 q.push({level + 1, position + speed, speed*2});
              
-            // wondering if reversing would workout
+            // wondering if reversing would get us to target
             if(speed > 0 && position + speed > target) {
-
                 q.push({level + 1, position, -1});
 
             } else if(speed < 0 && position + speed < target) {
-
                 q.push({level + 1, position, 1});
-
             }
-
         }
 
         return 0;
-
     }
 
 };
